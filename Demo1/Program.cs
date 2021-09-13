e83fd2a8dabd6731dd5c2eaf322ac7f40011532c
herkommer <m@herkommer.co.uk>
2021-09-13 09:56:59 +0200

    updated with more samples from today lesson

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -12,9 +12,35 @@ namespace Demo1
             Car myCar;          //Vi berättar att vi från stack tänker anv ett Car Objekt
             myCar = new Car();  //Här hamnar ett unikt objekt i minnet (Heap)
 
+            Console.WriteLine("Type= " + myCar.GetType().ToString());
+            Console.WriteLine(myCar.ToString());
+
+            Console.WriteLine("=============");
+
+            
             myCar.Color = "Red";
             myCar.Make = "Volvo";
             myCar.Model = "V70";
+
+            Console.WriteLine(myCar);           //tostring anropas
+            Console.WriteLine(myCar.MyData());  //MyData anropas
+            Console.WriteLine("=============");
+
+            Car yourCar;
+            yourCar = myCar;
+            yourCar.Color = "Blue";
+
+            int i = 10;
+            int j;
+            j = i;
+            j = 20;
+            Console.WriteLine("i= "+ i);
+            Console.WriteLine("j= " + j);
+
+            Console.WriteLine("myCar color: " + myCar.Color);
+            Console.WriteLine("yourCar color: " + yourCar.Color);
+
+            Console.WriteLine("=============");
 
             Console.WriteLine($"Din bil är en {myCar.Color} {myCar.Make} {myCar.Model}.");
 
@@ -45,6 +71,16 @@ namespace Demo1
 
             Console.WriteLine(MyCars[1].Model);
 
+            Console.WriteLine("================");
+            Console.WriteLine("List = " + MyCars.GetType());
+
+            //
+            Console.WriteLine("Vilken färg har din bil?");
+            Car newCar= new Car();
+            newCar.Color=Console.ReadLine();
+            MyCars.Add(newCar);
+            int x = MyCars.Count - 1;
+            Console.WriteLine(MyCars[x].Color);
         }
     }
 
@@ -61,6 +97,18 @@ namespace Demo1
         public string Model { get; set; }
         public string Make { get; set; }
 
+        //Jag ändrar hur klassen Car ska agera på ToString metoden
+        //det kallas för OVERRIDE
+        public override string ToString()
+        {
+            return $"Jag är en {Color} {Make} {Model}";
+        }
+
+        //Egen metod
+        public string MyData()
+        {
+            return $"I am a {Color} {Make} {Model}";
+        }
 
     }
 }
